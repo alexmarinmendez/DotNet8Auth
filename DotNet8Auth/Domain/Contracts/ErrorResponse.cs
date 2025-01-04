8f3e96e5b8c3dd35328dc7f8a132dfd9f460f57e
Alex Marín Méndez <alexmarinmendez@gmail.com>
2025-01-04 13:02:43 -0500

    Crear nueva clase ErrorResponse
    Se ha añadido una nueva clase `ErrorResponse` en el archivo `ErrorResponse.cs` dentro del espacio de nombres `DotNet8Auth.Domain.Contracts`. Esta clase contiene tres propiedades: `Title` (cadena de texto), `StatusCode` (entero) y `Message` (cadena de texto).

---
+++ DotNet8Auth/Domain/Contracts/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8Auth.Domain.Contracts
+{
+    public class ErrorResponse
+    {
+        public string Title { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+    }
+}
